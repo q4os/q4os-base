64043e3f30543e41cc3c986985b2e01e482d1f01
Q4OS Team <q4os@q4os.org>
2025-06-13 17:08:47 +0200

    Make get-q4os-version tool option to print nice output

--- src/filesystem/usr/share/apps/q4os_system/bin/print_qaptdistr.sh
+++ src/filesystem/usr/share/apps/q4os_system/bin/print_qaptdistr.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 if [ -z "$QAPTDISTR" ] ; then
-
   QAPTDISTR="bookworm" #fallback
 
   DDISTRIBUTOR="$( /usr/bin/lsb_release -i -s )"
@@ -15,14 +14,38 @@ if [ -z "$QAPTDISTR" ] ; then
     QAPTDISTR="trixie"
   elif [ "$DDISTRIBUTOR" = "Raspbian" ] && [ "$DCODENAME" = "bookworm" ] ; then
     QAPTDISTR="raspbian12"
-  elif [ "$DDISTRIBUTOR" = "Ubuntu" ] && [ "$DCODENAME" = "focal" ] ; then
-    QAPTDISTR="focal"
   elif [ "$DDISTRIBUTOR" = "Ubuntu" ] && [ "$DCODENAME" = "jammy" ] ; then
     QAPTDISTR="jammy"
   elif [ "$DDISTRIBUTOR" = "Ubuntu" ] && [ "$DCODENAME" = "noble" ] ; then
     QAPTDISTR="noble"
   fi
-
 fi
 
-echo "$QAPTDISTR"
+if [ "$OPT_CODENAME" = "1" ] ; then
+  if [ $QAPTDISTR = "bullseye" ] ; then
+    QCODENAME1="gemini"
+  elif [ $QAPTDISTR = "bookworm" ] ; then
+    QCODENAME1="aquarius"
+  elif [ $QAPTDISTR = "trixie" ] ; then
+    QCODENAME1="andromeda"
+  elif [ $QAPTDISTR = "raspbian12" ] ; then
+    QCODENAME1="aquarius"
+  elif [ $QAPTDISTR = "jammy" ] ; then
+    QCODENAME1="jammy"
+  elif [ $QAPTDISTR = "noble" ] ; then
+    QCODENAME1="noble"
+  else
+    QCODENAME1="unknown"
+  fi
+  OUTPUT1="$QCODENAME1"
+else
+  OUTPUT1="$QAPTDISTR"
+fi
+
+if [ "$OPT_UPPER_ALL" = "1" ] ; then
+  OUTPUT1="$( echo $OUTPUT1 | awk '{ print toupper($0) }' )"
+elif [ "$OPT_UPPER_FIRST" = "1" ] ; then
+  OUTPUT1="$( echo $OUTPUT1 | awk '{ print substr(toupper($0),1,1) substr($0,2) }' )"
+fi
+
+echo "$OUTPUT1"
